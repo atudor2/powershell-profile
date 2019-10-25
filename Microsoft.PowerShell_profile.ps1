0206baf0f5130135536bf500a29206fa3cd90d37
Alistair Tudor <kryten94@gmail.com>
2019-10-25 11:26:25 +0200

    Add touch() function to profile to mimic *nix 'touch'

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -25,6 +25,19 @@ Set-Alias gitlog2 Show-GitLog2;
 ###########################################################################
 # Custom functions:                                                       #
 ###########################################################################
+function touch {
+    Param(
+      [Parameter(Mandatory=$true)]
+      [string]$Path
+    )
+  
+    if (Test-Path -LiteralPath $Path) {
+      (Get-Item -Path $Path).LastWriteTime = Get-Date
+    } else {
+      New-Item -Type File -Path $Path
+    }
+}
+
 function Show-GitLog1() {
     & git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all;
 }
